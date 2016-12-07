8d588fc45c95186a14b411f388878b6054a91327
Tunscopi <ayotundeodejayi@yahoo.com>
2016-12-07 00:51:55 -0500

    perfected reading in asset prices

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <math>
+#include <math.h>
 #include <fstream>
 using namespace std;
 
@@ -11,35 +11,37 @@ int main() {
     ifstream file_reader;
 
     // Asset prices
-    int stocks[no_years+1][width], fixed_1[no_years][width], fixed_2[no_years-1][width], fixed_3[no_years-2][width], fixed_4[no_years-3][width];
-    int real_estate[no_years+1][width], ecommerce[no_years+1][width], farms[no_years+1][width];
+    float stocks[no_years+1][width], fixed_1[no_years][width], fixed_2[no_years-1][width], fixed_3[no_years-2][width], fixed_4[no_years-3][width];
+    float real_estate[no_years+1][width], ecommerce[no_years+1][width], farms[no_years+1][width];
 
     // Read-in data
     file_reader.open("AssetPrices.txt");
     
     for (int i=0; i<no_years+1; i++){
-        for (int i=0; j<width; j++)
+        for (int j=0; j<width; j++)
              file_reader >> stocks[i][j];
     }
-    int index_control = no_years;
+    int index = 0;
     for (int i=0; i<no_years; i++) {
-        for (int index = 0; index<index_control; index++) {
-            for (int j=0; j<width; j++) {
+            for (int j=0; j<width; j++) 
                 file_reader >> fixed_1[i][j];
-                if (index > 0) file_reader >> fixed_2[i][j];
-                if (index > 1) file_reader >> fixed_3[i][j];
-                if (index > 2) file_reader >> fixed_4[i][j];  
-            }
-        }
+            if (index < 3) {for (int j = 0; j<width; j++)
+                file_reader >> fixed_2[i][j];}
+            if (index < 2) {for (int j = 0; j<width; j++)
+                file_reader >> fixed_3[i][j];}
+            if (index < 1) {for (int j = 0; j<width; j++) 
+                file_reader >> fixed_4[i][j];}  
+
+            index++;
     }
                 
    // Test
    for (int i = 0; i<no_years; i++) {
      for (int j = 0; j< width; j++) {
-       cout << stocks[i][j] << " ";
+       cout << fixed_1[i][j] << " ";
      }
    cout << endl;
    }  
-
-    system("pause");
+   
+  return 0;
 }
